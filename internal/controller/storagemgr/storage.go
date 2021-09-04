5867c099be9e4fd4376b657d0a40acaa64086544
LeeSeungHui <hithere1012@naver.com>
2021-09-05 01:57:06 +0900

    Remove getDeviceID function in storage.go file
    Signed-off-by: LeeSeungHui <hithere1012@naver.com>

--- internal/controller/storagemgr/storage.go
+++ internal/controller/storagemgr/storage.go
@@ -212,10 +212,3 @@ func saveYaml() (err error) {
 	return
 }
 
-func getDeviceID() (string, error) {
-	UUIDv4, err := ioutil.ReadFile(deviceIDFilePath)
-	if err != nil {
-		return "", err
-	}
-	return string(UUIDv4), nil
-}
